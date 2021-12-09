59b52a2c3e656ea87aab37db093d812cf627d3de
skytomo <tomo1kawa.fullerene@gmail.com>
2021-12-09 15:21:56 +0900

    Reset and re-introduce React + TypeScript 💣

--- src/actions.ts
+++
@@ -1,9 +0,0 @@
-import { actionCreatorFactory } from 'typescript-fsa';
-
-const actionCreator = actionCreatorFactory()
-
-export const TextInputActions = {
-  updateTextInputValue: actionCreator<string>('ACTIONS_UPDATE_TEXT_INPUT_VALUE'),
-  updateSelectedValue: actionCreator<string>('ACTION_UPDATE_SELECTED_VALUE'),
-  updateClickCount: actionCreator('ACTION_UPDATE_CLICK_COUNT')
-}
